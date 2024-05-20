b7a6b17bc71949fd4309f5698d9daef785e20f0a
Stella-owo-er <101204024+Stella-owo-er@users.noreply.github.com>
2024-05-20 20:08:45 +0700

    add new libray and done for forgot password

--- src/java/Controller/ForgotPassword.java
+++ src/java/Controller/ForgotPassword.java
@@ -4,14 +4,27 @@
  */
 package Controller;
 
-import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Properties;
+import java.util.Random;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.PrintWriter;
+
+
 
 /**
  *
@@ -73,7 +86,76 @@ public class ForgotPassword extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       // processRequest(request, response);
+       //Gửi mail đơn giản sử dụng SMTP của Gmail với kết nối SSL (Secure Sockets Layer)
+       String email = request.getParameter("email");
+        
+        RequestDispatcher dispatcher = null;
+        
+        int otpvalueLength = 6; // ma otp co do dai la 6
+        
+        Random rand = new Random();
+        
+        String string = "0123456789A-Za-z"; 
+        
+        String randomOtp = ""; //luu tru opt duoc tao ra ngau nhien
+        
+        HttpSession mySession = request.getSession();
+        
+        if (email != null || !email.equals("")) {
+            for (int i = 0; i < otpvalueLength; i++) {
+                char c = string.charAt(rand.nextInt(string.length())); // moi vong lap se co 1 ki tu otp duoc sinh ra ngau nhien
+                randomOtp = randomOtp + c;    // sau do duoc noi voi ki tu sinh ra ngau nhien truoc do cho den khi chuoi kí tu co do dai = 6
+            }
+            Cookie cookie = new Cookie("otpR", randomOtp); // tao 1 cookie voi gia tri duoc tao cung la randomOtp
+            
+            cookie.setMaxAge(1*60); // max = 1m
+            
+            response.addCookie(cookie);
+            
+            String to = email; // to luu tru dia chi nguoi nhan, co the co nhieu nguoi nhan 
+            
+            
+            // Get the session object
+            Properties props = new Properties();
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
+            
+            Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("bnvqm1721@gmail.com", "tgqwyawkaytmqvka");
+                }
+            });
+            try {
+                MimeMessage message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(email));
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+                message.setSubject("Request to reset password ");
+                message.setText("Hi, for security, please verify your account with the OPT below. " 
+                        + "Your OTP is: " + randomOtp + ". " + "Click the link to enter otp: " + "http://localhost:9999/SWP391_Draft1/enterotp.jsp");
+                Transport.send(message);
+                System.out.println("message sent successfully");
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+
+            
+            //=======================================================
+            
+            
+            
+            dispatcher = request.getRequestDispatcher("ForgotPAssword.jsp");
+            request.setAttribute("message", "OTP is sent to your email");
+            mySession.setAttribute("otp", randomOtp);
+            mySession.setAttribute("email", email);
+            dispatcher.forward(request, response);
+        } 
+
+    
+
     }
 
     /**
--- src/java/DBContext/DBContext.java
+++ src/java/DBContext/DBContext.java
@@ -28,7 +28,7 @@ public class DBContext {
         try {
             String user = "haunt";
             String pass = "01669665445";
-            String url = "jdbc:sqlserver://DESKTOP-V91QVMQ:3306;databaseName=SaleComputer";
+            String url = "jdbc:sqlserver://DESKTOP-V91QVMQ:3306;databaseName=S391";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
